987119ef8dc76719c08971d6bbe6974696b54153
Yannis <yannis.bouacida@appliedtechnology.se>
2022-03-22 08:50:38 +0100

    modify time on contract to 15 seconds for spam

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -37,7 +37,7 @@ contract WavePortal {
          * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
          */
         require(
-            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            lastWavedAt[msg.sender] + 15 seconds < block.timestamp,
             "Wait 15m"
         );
 
